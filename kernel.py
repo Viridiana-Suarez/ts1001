98da3063e6513a6651f6bb30e6616c05f8a1e2b0
Clar1ty1 <90118495+Clar1ty1@users.noreply.github.com>
2023-03-23 10:53:46 -0600

    Added readme

--- kernel.py
+++ kernel.py
@@ -8,14 +8,6 @@ from matplotlib import pyplot
 import cv2
 
 cap = cv2.VideoCapture('video.mp4')
-while True:
-    ret, frame = cap.read()
-    if not ret: break # break if no next frame
-    
-    cv2.imshow(frame) # show frame
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'): # on press of q break
-        break
 
 def mexhat(x,y,s):
     return 1/(np.pi*s**4)*(1-1/2*(x**2+y**2)/s**2)*np.exp(-(x**2+y**2)/(2*s**2))
@@ -26,7 +18,6 @@ def gauss2d(x,y,s):
 def genGaussKernel(n, size,s, fnc):
     x=np.linspace(-n,n,size)
     y=np.linspace(-n,n,size)
-
     g=np.zeros((size,size))
 
     for i,xi in enumerate(x):
@@ -34,3 +25,13 @@ def genGaussKernel(n, size,s, fnc):
             g[i,j]=fnc(xi,yi,s)
     
     return g
+frameArr = []
+while cap.isOpened():
+    ret, frame = cap.read()
+    I = frame;
+    I = np.asarray(I);
+
+
+    if not ret: 
+        break # break if no next frame
+print(frameArr)
